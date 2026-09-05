ab56d257fb9988416f01306c2de2189870a26bd0
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-09-05 11:28:53 +0530

    Mac backup: 2026-09-05 11:28:53

--- cpp_source/test.cpp
+++ cpp_source/test.cpp
@@ -5,3 +5,22 @@ int main(void) {
     std::cout << "Notlo Modda" << std::endl;
     return 0;
 }
+
+/* Apple Compiler:
+/opt/homebrew/opt/llvm/bin/clang++ \
+  -std=c++20 \
+  -O3 \
+  -march=native \
+  -I/opt/homebrew/include \
+  -I/opt/homebrew/opt/libomp/include \
+  -L/opt/homebrew/opt/libomp/lib \
+  -fopenmp \
+  test.cpp \
+  -o executable \
+  -framework Accelerate
+*/
+
+// ----------------------------// 
+
+/*Linux(Debian) Compiler:
+clang++ -std=c++20 -O3 -march=native -fopenmp -lblas test.cpp -o executable*/
