fbf8741795e9fac45813030043b7dc2ebf537e2e
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-09-05 15:02:28 +0530

    Mac backup: 2026-09-05 15:02:28

--- cpp_source/matrix_addn.cpp
+++ cpp_source/matrix_addn.cpp
@@ -5,9 +5,25 @@
 //  Created by Abhishek Chintada on 05/09/26.
 // See test.cpp for execution standards.
 
-#include <blaze/Math.h>
+#include <blaze/Blaze.h>
+#include <iostream>
 #include <cstddef>
 
+void print_Vector(blaze::DynamicVector x) {
+    std::cout << "[";
+    for(size_t i {}, i <= 3, i++) {
+        std::cout << x[i];
+    }
+    std::cout << "]";
+}
+
 int main(void) {
+    blaze::DynamicVector<double> b {4.0, 5.0, 6.0};
+    blaze::DynamicVector<double> a {1.0, 2.0, 3.0};
+    blaze::DynamicVector<double> res(3, 0.0);
+    blaze::DynamicMatrix<double, blaze::rowMajor> {{1.0, 2.0, 3.0},{4.0, 5.0, 6.0},{7.0, 8.0, 9.0}};
+    blaze::DynamicVector<double> c {0.0, 0.0, 0.0};
+    c = a + b;
+    print_Vector(c);
     return 0;
 }
