6e2cf7b4b5073176d57cf8002233eb978fbf331f
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-09-06 19:13:19 +0530

    Auto-commit from Arch setup: 2026-09-06 19:13:19

--- cpp_source/blaze_essentials.hpp
+++ cpp_source/blaze_essentials.hpp
@@ -3,3 +3,14 @@
 #include <blaze/Blaze.h>
 #include <iostream>
 
+
+template<typename VectorType>
+void print_Vector(const VectorType& x, size_t N) {
+    std::cout << "[ ";
+    for(size_t i {}; i < N; i++) {
+        std::cout << x[i] << "  ";
+    }
+    std::cout << "]" << std::endl;
+}
+
+
--- cpp_source/cppcon2016.cpp
+++ cpp_source/cppcon2016.cpp
@@ -1,17 +1,43 @@
-#include <iostream>
+#include "blaze_essentials.hpp"
 #include <random>
-#include <blaze/Blaze.h>
+#include <chrono>
 
 int main(void) {
-    size_t N = 10;
+    // blaze implementation
+    blaze::setNumThreads(8);
+    auto start_blaze = std::chrono::high_resolution_clock::now();
+    size_t N = 10000000;
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> distribution(1, 20);
+    std::uniform_real_distribution<double> distribution(1, 20);
     blaze::DynamicVector<double, blaze::columnVector> a(N), b(N), c(N);
     for(size_t i {}; i < N; i++) {
         a[i] = distribution(gen);
         b[i] = distribution(gen);
     }
-    c = a+b;
+    auto end_blaze = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_blaze = end_blaze - start_blaze;
+    // print_Vector(a, N);
+    // print_Vector(b, N);
+    c = a*b;
+    // print_Vector(c, N);
+    std::cout << "<console> Duration for blaze : " << elapsed_blaze.count() << std::endl;
+    
+
+
+    // normal std vector implementation
+    auto start_vec = std::chrono::high_resolution_clock::now();
+    std::vector<double> v1(N, 0);
+    std::vector<double> v2(N, 0);
+    std::vector<double> sum(N, 0);
+    for(size_t i {}; i < N; i++) {
+        v1[i] = distribution(gen);
+        v2[i] = distribution(gen);
+        sum[i] = v1[i] + v2[i];
+    }
+    auto end_vec = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_vec = end_vec - start_vec;
+    std::cout << "<console> Duration for vector : " << elapsed_vec.count() << std::endl;
+
     return 0;
 }
