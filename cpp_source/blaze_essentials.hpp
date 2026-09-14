b640f2b66d6d245f93422ceb3e4a79b9ffabd92e
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-09-14 18:29:32 +0530

    Auto-commit from Arch setup: 2026-09-14 18:29:32

--- cpp_source/blaze_essentials.hpp
+++ cpp_source/blaze_essentials.hpp
@@ -3,22 +3,5 @@
 #include <blaze/Blaze.h>
 #include <iostream>
 
-
-template<typename VectorType>
-void print_Vector(const VectorType& x, size_t N) {
-    std::cout << "[ ";
-    for(size_t i {}; i < N; i++) {
-        std::cout << x[i] << "  ";
-    }
-    std::cout << "]" << std::endl;
-}
-
-
-void print_Matrix(const blaze::DynamicMatrix<double, blaze::rowMajor>& x, size_t N) {
-    std::cout << "[" ;
-    for(size_t i {}; i < x.rows(); i++) {
-        if(i != 0) std::cout << " ";
-        print_Vector(blaze::row(x, i), N);
-    }
-}
-
+// printing templates not required, use std::cout -> Already overloaded.
+// Taking user input -> loops are the best bet.
