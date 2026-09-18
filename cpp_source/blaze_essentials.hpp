6726592ba3490619359c2318bd86b46ad2367f10
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-09-18 15:14:34 +0530

    Mac backup: 2026-09-18 15:14:34

--- cpp_source/blaze_essentials.hpp
+++ cpp_source/blaze_essentials.hpp
@@ -4,13 +4,6 @@
 #include <iostream>
 
 
-template<typename VectorType>
-void print_Vector(const VectorType& x, size_t N) {
-    std::cout << "[ ";
-    for(size_t i {}; i < N; i++) {
-        std::cout << x[i] << "  ";
-    }
-    std::cout << "]" << std::endl;
-}
+
 
 
